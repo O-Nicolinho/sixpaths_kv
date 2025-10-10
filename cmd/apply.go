8fcab66d4282b5128325c82fb2ebda0d6827712a
Nicolinho <ritsuko_0@protonmail.com>
2025-10-10 11:47:51 -0400

    Started implementing the Apply function to execute commands for the KVS

--- cmd/apply.go
+++ cmd/apply.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"errors"
+)
+
 type Command struct {
 	Instruct CommandType
 	ClientID string
@@ -17,9 +21,59 @@ const (
 )
 
 func validType(t CommandType) bool {
-	return t == 1 || t == 2 // these are the only valid CommandType nums
+	return t == CmdPut || t == CmdDelete // these are the only valid CommandType nums
 }
 
-func (s *Store) Apply(cmd Command, index uint64) {
+type ApplyResult struct {
+	Success   bool
+	PrevValue []byte // To see what was deleted or overwritten
+	LogIndex  uint64
+}
+
+func (s *Store) Apply(cmd Command, logindex uint64) (ApplyResult, error) {
+
+	r := ApplyResult{
+		Success:   false,
+		PrevValue: []byte{},
+		LogIndex:  logindex,
+	}
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if logindex != s.lastlogi+1 {
+		return r, errors.New("error: new apply request index is not equal to last log index + 1")
+	}
+
+	// we check whether the SEQ num provided by the cmd is the equal (or older) than
+	// the last SEQ num provided by this particular client's request.
+	// Since SEQ nums are unique per request, if these two are the same
+	// then we are dealing with a duplicate request.
+	if cmd.Seq <= s.dedupMap[cmd.ClientID].seq {
+		r.PrevValue = s.dedupMap[cmd.ClientID].result.PrevValue
+		// return previous ApplyResult if we are dealing with a dupe
+		return s.dedupMap[string(cmd.Key)].result, nil
+
+	}
+
+	switch cmd.Instruct {
+	case CmdPut: // Put
+		return r, nil // stub
+
+	case CmdDelete: // Delete
+		v, ok := s.kv[string(cmd.Key)]
+		if !ok {
+			return r, errors.New("error: value at key in store is already empty; nothing to delete")
+		}
+		// we make a copy of the byte slice to not alter it
+		prev := append([]byte(nil), v...)
+
+		delete(s.kv, string(cmd.Key))
+		r.PrevValue = prev
+		r.Success = true
+		s.lastlogi += 1
+		return r, nil
+	default:
+		return r, errors.New("error: Apply failed, invalid cmd passed.")
+	}
 
 }
--- cmd/store.go
+++ cmd/store.go
@@ -3,7 +3,13 @@ package main
 import "sync"
 
 type Store struct {
-	kv    map[string][]byte
-	lasti uint64
-	mu    sync.Mutex
+	kv       map[string][]byte
+	lastlogi uint64
+	mu       sync.Mutex
+	dedupMap map[string]Dedup // map where key=clientID string, value=dedup
+}
+
+type Dedup struct {
+	seq    uint64
+	result ApplyResult
 }
