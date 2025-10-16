4868d3539594230b2635549f53e3331ada57f398
Nicolinho <ritsuko_0@protonmail.com>
2025-10-15 21:05:08 -0400

    Working on WAL implementation

--- internal/wal.go
+++ internal/wal.go
@@ -1,18 +1,40 @@
 package sixpaths_kvs
 
-import "os"
+import (
+	"bufio"
+	"os"
+	"path/filepath"
+)
 
 type WAL struct {
 	f      *os.File
 	path   string
 	offset int64
+	bw *bufio.Writer
+	hdrLen int
 }
+
+
 
 type Record struct {
 	LogIndex uint64
 	Cmd      Command
 }
 
-func NewWal(path string) (WAL, err) {
+func NewWAL(path string) (*WAL, error) {
+	var walHeader = []byte("WALv1-BE\x00")
+
+	dir := filepath.Dir(path)
+	err := os.MkdirAll(dir, 0o755)
+	// checks whether directory specified by path exists.
+	if err != nil {
+		return nil, err
+	}
+
+	f, err := os.OpenFile(path, os.0_RDWR|os.0_CREATE, 0o644)
+
+	if err != nil {
+		return nil, err
+	}
 
 }
