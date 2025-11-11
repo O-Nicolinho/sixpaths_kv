b8eb433ad2d90872579c38063120f9ea4a2b4f7e
Nicolinho <ritsuko_0@protonmail.com>
2025-11-10 19:24:24 -0500

    Metrics.go

--- internal/metrics.go
+++ internal/metrics.go
@@ -1,9 +1,27 @@
 package sixpaths_kvs
 
-func IncExec()  {}
-func IncPut()   {}
-func IncDel()   {}
-func IncDedup() {}
+import "sync/atomic"
+
+var (
+	execTotal uint64
+	putTotal  uint64
+	delTotal  uint64
+	dedupHits uint64
+)
+
+func IncExec() {
+	atomic.AddUint64(&execTotal, 1)
+}
+
+func IncPut() {
+	atomic.AddUint64(&putTotal, 1)
+}
+func IncDel() {
+	atomic.AddUint64(&delTotal, 1)
+}
+func IncDedup() {
+	atomic.AddUint64(&dedupHits, 1)
+}
 
 type MetricsSnapshot struct {
 	ExecTotal uint64 `json:"exec_total"`
@@ -13,5 +31,10 @@ type MetricsSnapshot struct {
 }
 
 func Snapshot() MetricsSnapshot {
-	return MetricsSnapshot{}
+	return MetricsSnapshot{
+		ExecTotal: atomic.LoadUint64(&execTotal),
+		PutTotal:  atomic.LoadUint64(&putTotal),
+		DelTotal:  atomic.LoadUint64(&delTotal),
+		DedupHits: atomic.LoadUint64(&dedupHits),
+	}
 }
